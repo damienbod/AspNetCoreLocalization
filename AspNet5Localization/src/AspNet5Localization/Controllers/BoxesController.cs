69d370d8244e6ee3a65383f2bd7b17dc0a3daf6c
damienbod <damien_bod@hotmail.com>
2015-10-24 18:37:45 +0200

    post method

--- AspNet5Localization/src/AspNet5Localization/Controllers/BoxesController.cs
+++ AspNet5Localization/src/AspNet5Localization/Controllers/BoxesController.cs
@@ -10,12 +10,6 @@ namespace AspNet5Localization.Controllers
     [Route("api/[controller]")]
     public class BoxesController : Controller
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
@@ -27,6 +21,14 @@ namespace AspNet5Localization.Controllers
             return Ok(new Box() { Id = id, Height = 10, Length = 10, Width=10 });
         }
 
+        /// <summary>
+        /// http://localhost:5000/api/boxes?culture=it-CH
+        /// Content-Type: application/json
+        /// 
+        /// { "Id":7,"Height":10,"Width":10,"Length":1000}
+        /// </summary>
+        /// <param name="box"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Post([FromBody]Box box)
         {
