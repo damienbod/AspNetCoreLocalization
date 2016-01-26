403c299f12a4f05738bc1583f4fa33e473765407
damienbod <damien_bod@hotmail.com>
2016-01-26 21:33:51 +0100

    code clean up

--- AspNet5Localization/src/AspNet5Localization/Startup.cs
+++ AspNet5Localization/src/AspNet5Localization/Startup.cs
@@ -43,31 +43,9 @@ namespace AspNet5Localization
                             new CultureInfo("it-CH")
                         };
 
-
-
-                        // State what the default culture for your application is. This will be used if no specific culture
-                        // can be determined for a given request.
                         options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
-
-                        // You must explicitly state which cultures your application supports.
-                        // These are the cultures the app supports for formatting numbers, dates, etc.
                         options.SupportedCultures = supportedCultures;
-
-                        // These are the cultures the app supports for UI strings, i.e. we have localized resources for.
                         options.SupportedUICultures = supportedCultures;
-
-                        // You can change which providers are configured to determine the culture for requests, or even add a custom
-                        // provider with your own logic. The providers will be asked in order to provide a culture for each request,
-                        // and the first to provide a non-null result that is in the configured supported cultures list will be used.
-                        // By default, the following built-in providers are configured:
-                        // - QueryStringRequestCultureProvider, sets culture via "culture" and "ui-culture" query string values, useful for testing
-                        // - CookieRequestCultureProvider, sets culture via "ASPNET_CULTURE" cookie
-                        // - AcceptLanguageHeaderRequestCultureProvider, sets culture via the "Accept-Language" request header
-                        //options.RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(async context =>
-                        //{
-                        //  // My custom request culture logic
-                        //  return new ProviderCultureResult("en");
-                        //}));
                     });
         }
 
