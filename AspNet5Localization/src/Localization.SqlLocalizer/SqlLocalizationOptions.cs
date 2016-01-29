e97c4bb07f7b2b6c6de5152c2c42c3f76d82bb0f
damienbod <damien_bod@hotmail.com>
2016-01-29 10:07:53 +0100

    code clean up

--- AspNet5Localization/src/Localization.SqlLocalizer/SqlLocalizationOptions.cs
+++ AspNet5Localization/src/Localization.SqlLocalizer/SqlLocalizationOptions.cs
@@ -10,7 +10,7 @@ namespace Localization.SqlLocalizer
     public class SqlLocalizationOptions
     {
         /// <summary>
-        /// If UseOnlyPropertyNames is false, this propety can be used to define keys with full type names or just the name of the class
+        /// If UseOnlyPropertyNames is false, this property can be used to define keys with full type names or just the name of the class
         /// </summary>
         public bool UseTypeFullNames { get; set; }
 
