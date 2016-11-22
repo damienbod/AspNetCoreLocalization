216c84a728681fdc44618c5883a9967076e8a741
damienbod <damien_bod@hotmail.com>
2016-11-22 14:52:43 +0100

    code clean up

--- AspNet5Localization/src/Localization.SqlLocalizer/DbStringLocalizer/SqliteCreateLocalizationRecord.sql
+++ AspNet5Localization/src/Localization.SqlLocalizer/DbStringLocalizer/SqliteCreateLocalizationRecord.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "LocalizationRecords" (
     "Id" INTEGER NOT NULL CONSTRAINT "PK_DataEventRecord" PRIMARY KEY AUTOINCREMENT,
     "Key" TEXT,
-	"ResourceKey" TEXT,
+    "ResourceKey" TEXT,
     "Text" TEXT,
     "LocalizationCulture" TEXT,
     "UpdatedTimestamp" TEXT NOT NULL
