e0b57a80d4805a7ad94293c68ac92c006c1f6fc6
damienbod <damien_bod@hotmail.com>
2016-12-05 07:47:28 +0100

    Adding the new configuration for the library

--- AspNet5Localization/src/Localization.SqlLocalizer/SqlLocalizationOptions.cs
+++ AspNet5Localization/src/Localization.SqlLocalizer/SqlLocalizationOptions.cs
@@ -11,5 +11,10 @@
         /// This can be used to use only property names to find the keys
         /// </summary>
         public bool UseOnlyPropertyNames { get; set; }
+
+        /// <summary>
+        /// Returns only the Key if the value is not found. If set to false, the search key in the database is returned.
+        /// </summary>
+        public bool ReturnOnlyKeyIfNotFound { get; set; }
     }
 }
