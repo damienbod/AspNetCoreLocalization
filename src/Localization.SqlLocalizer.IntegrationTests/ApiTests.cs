bcc5a51d83d122e18dbcff9fa5c08d9d0c816724
damienbod <damien_bod@hotmail.com>
2017-05-13 22:48:56 +0200

    Init test structure

---
+++ src/Localization.SqlLocalizer.IntegrationTests/ApiTests.cs
@@ -0,0 +1,78 @@
+﻿using Localization.SqlLocalizer.DbStringLocalizer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreProtobuf.IntegrationTests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class ApiTests
+    {
+
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public ApiTests()
+        {
+        }
+
+        [Fact]
+        public async Task GetProtobufDataAsString()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(services =>
+                {
+                    var sqlConnectionString = Configuration["DbStringLocalizer:ConnectionString"];
+
+                    services.AddDbContext<LocalizationModelContext>(options =>
+                        options.UseSqlite(
+                            sqlConnectionString,
+                            b => b.MigrationsAssembly("AspNetCoreLocalization")
+                        )
+                    );
+
+                    var useTypeFullNames = false;
+                    var useOnlyPropertyNames = false;
+                    var returnOnlyKeyIfNotFound = false;
+                    var createNewRecordWhenLocalisedStringDoesNotExist = false;
+
+
+                    // Requires that LocalizationModelContext is defined
+                    // _createNewRecordWhenLocalisedStringDoesNotExist read from the dev env. 
+                    services.AddSqlLocalization(options => options.UseSettings(
+                        useTypeFullNames,
+                        useOnlyPropertyNames,
+                        returnOnlyKeyIfNotFound,
+                        createNewRecordWhenLocalisedStringDoesNotExist));
+
+                })
+                .Configure(app =>
+                {
+                   
+                    app.Run(context =>
+                    {
+                        var requestCultureFeature = context.Features.Get<IRequestCultureFeature>();
+                        var requestCulture = requestCultureFeature.RequestCulture;
+                        Assert.Equal("ar-SA", requestCulture.Culture.Name);
+                        return Task.FromResult(0);
+                    });
+                });
+
+            using (var server = new TestServer(builder))
+            {
+                var client = server.CreateClient();
+                client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("jp,ar-SA,en-US");
+                var count = client.DefaultRequestHeaders.AcceptLanguage.Count;
+                var response = await client.GetAsync(string.Empty);
+                Assert.Equal(3, count);
+            }
+        }
+
+    }
+}
---
+++ src/Localization.SqlLocalizer.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e94b6cae-5e84-43e8-89dc-7af19a781fbb")]
