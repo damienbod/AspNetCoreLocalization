072566630abd3a24adc374cbc4ddf22c97664084
damienbod <damien_bod@hotmail.com>
2017-05-14 18:33:52 +0200

    nuget package

--- src/Localization.SqlLocalizer/Properties/AssemblyInfo.cs
+++ src/Localization.SqlLocalizer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Localization.SqlLocalizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ASP.NET Core localization with SQL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Localization.SqlLocalizer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
